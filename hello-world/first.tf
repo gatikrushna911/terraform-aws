eba1fe052f1ef31aa9342eec501f1e0b311b9fcf
gatikrushna911 <gatikrushna911@gmail.com>
2026-05-21 04:00:56 +0530

    hellow world programme

---
+++ hello-world/first.tf
@@ -0,0 +1,10 @@
+//block  "label1" label2 {
+//identifier = expression
+//}
+
+// this is the first comment 
+## this is another way to write a comment 
+
+output hello1 {
+    value = "hello world"
+}
